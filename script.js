1ffc903bd7acf8eeb525f65d8e3c30af24680165
Kaleidosport <lambert.ant14@gmail.com>
2021-10-18 14:25:37 +0200

    Intermediate commit

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 let counter = 0
-
-let additionalCookies = 0
+let unitsPerSec = 0
+let additionalUnits = []
 
 const AUGMENTERS = [
     ["Clicker", x, y, z], ["Astronaut", x, y, z],
     ["Spacecraft", x, y, z], ["Space shuttle", x, y, z],
     ["NASA", x, y, z], ["Tony Stark", x, y, z]
 ]
+
+class Bonuses {
+    constructor(bonusName, bonusAmount, bonusPrice, bonusTotal) {
+        this.bonusName = bonusName
+        this.bonusAmount = bonusAmount
+        this.bonusPrice = bonusPrice
+        this.bonusTotal = bonusTotal
+    }
+
+    functionbyZac() {
+        additionalUnits += this.bonusAmount * this.bonusTotal
+    } // Variable dedicated to the amount of units by second 
+
+    functionByZac2() {
+        counter > this.bonusPrice ? counter -= this.bonusPrice : counter
+        this.bonusTotal++
+    } // Variable dedicated to buying items and deducting from total
+}
+
