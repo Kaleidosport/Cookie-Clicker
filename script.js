6f42f0a2890b1e4bf1ca39edefbdc75b9a051e72
Kaleidosport <lambert.ant14@gmail.com>
2021-10-18 11:04:57 +0200

    Initial JS commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let counter = 0
+
+let additionalCookies = 0
+
+const AUGMENTERS = [
+    ["Clicker", x, y, z], ["Astronaut", x, y, z],
+    ["Spacecraft", x, y, z], ["Space shuttle", x, y, z],
+    ["NASA", x, y, z], ["Tony Stark", x, y, z]
+]
